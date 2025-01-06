98814a8071cd76d1f6bc0f0382cae1e96f1b1557
Askthemaster100 <t0527168987@gmail.com>
2025-01-07 00:34:33 +0200

    IRepository

---
+++ DentalClinic.Core/Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DentalClinic.Core.Repositories
+{
+    public interface IRepository<T> where T:class
+    {
+        DbSet<T> GetAll();
+
+        IEnumerable<T> GetByid(int id);
+        Task<bool> Post(T entity);
+        Task<bool> Put(int id, T entity);
+        Task<bool> Delete(int id);
+    }
+   
+}
